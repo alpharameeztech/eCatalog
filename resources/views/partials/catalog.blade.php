7e9ba12601a63bbb561243a4205d0c623e5c7dac
alpharameeztech <rameezisrarulhaq@gmail.com>
2020-04-22 21:28:46 +0500

    Create useable sections for catalog,nav and logos

---
+++ resources/views/partials/catalog.blade.php
@@ -0,0 +1,62 @@
+<div class="grid grid-cols-4 gap-4 mt-4">
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img class="w-full" src="https://cdn.wowdeals.me/uploads/catalogues/issues/104/12815/cover/300x406/1587538973.jpg" alt="Sunset in the mountains">
+        <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p class="text-gray-700 text-base">
+                Super Deal
+            </p>
+            <p class="font-extrabold text-gray-700 text-base">
+                22 - 28 Apr
+            </p>
+            <p class="text-gray-700 text-base">
+                By <a class="no-underline hover:underline text-blue-500 text-lg" href="">Chris</a>
+            </p>
+        </div>
+    </div>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img class="w-full" src="https://cdn.wowdeals.me/uploads/catalogues/issues/33/12814/cover/300x406/1587536376.jpg" alt="Sunset in the mountains">
+        <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p class="text-gray-700 text-base">
+                Super Deal
+            </p>
+            <p class="font-extrabold text-gray-700 text-base">
+                22 - 28 Apr
+            </p>
+            <p class="text-gray-700 text-base">
+                By <a class="no-underline hover:underline text-blue-500 text-lg" href="">Chris</a>
+            </p>
+        </div>
+    </div>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img class="w-full" src="https://cdn.wowdeals.me/uploads/catalogues/issues/104/12815/cover/300x406/1587538973.jpg" alt="Sunset in the mountains">
+        <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p class="text-gray-700 text-base">
+                Super Deal
+            </p>
+            <p class="font-extrabold text-gray-700 text-base">
+                22 - 28 Apr
+            </p>
+            <p class="text-gray-700 text-base">
+                By <a class="no-underline hover:underline text-blue-500 text-lg" href="">Chris</a>
+            </p>
+        </div>
+    </div>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img class="w-full" src="https://cdn.wowdeals.me/uploads/catalogues/issues/33/12814/cover/300x406/1587536376.jpg" alt="Sunset in the mountains">
+        <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p class="text-gray-700 text-base">
+                Super Deal
+            </p>
+            <p class="font-extrabold text-gray-700 text-base">
+                22 - 28 Apr
+            </p>
+            <p class="text-gray-700 text-base">
+                By <a class="no-underline hover:underline text-blue-500 text-lg" href="">Chris</a>
+            </p>
+        </div>
+    </div>
+</div>
---
+++ resources/views/partials/logos.blade.php
@@ -0,0 +1,22 @@
+<div class="grid grid-cols-6 gap-4 mt-8">
+
+    <div>
+        <img class="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains">
+    </div>
+    <div>
+        <img class="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains">
+    </div>
+    <div>
+        <img class="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains">
+    </div>
+    <div>
+        <img class="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains">
+    </div>
+    <div>
+        <img class="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains">
+    </div>
+    <div>
+        <img class="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains">
+    </div>
+
+</div>
