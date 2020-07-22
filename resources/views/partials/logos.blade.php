4f263435fe6e2b5fee77e5427aa1cc4b997ae403
alpharameeztech <rameezisrarulhaq@gmail.com>
2020-07-23 01:57:16 +0500

    Add the hyper link on the store icon on the home page

--- resources/views/partials/logos.blade.php
+++ resources/views/partials/logos.blade.php
@@ -4,12 +4,14 @@
         @foreach ($stores as $store)
         @if ($store->featured)
             <div class="col-sm-2">
-                <img class="w-full" src="https://ecatalog.s3-ap-southeast-1.amazonaws.com/{{$store->image}}" alt="Sunset in the mountains">
+                <a href="/store/{{$store->slug}}">
+                    <img class="w-full" src="https://ecatalog.s3-ap-southeast-1.amazonaws.com/{{$store->image}}" alt="Sunset in the mountains">
+                </a>
             </div>
         @endif
-      
+
     @endforeach
     </div>
-   
+
 
 </div>
