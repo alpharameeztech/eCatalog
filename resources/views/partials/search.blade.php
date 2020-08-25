58b7b5366ccdd1d8ac76a090313a26ef9bb49930
alpharameeztech <rameezisrarulhaq@gmail.com>
2020-08-25 23:45:16 +0500

    Update the layout of the search on the arabic version

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -5,8 +5,15 @@
             <div class="col-sm-12">
                 <form method="post" class="example searchForm" action="{{ route('search.catalogs') }}" style="margin:auto">
                     @csrf
-                    <input type="text" placeholder="{{ trans('index.find_your_catalog') }}"  name="search">
-                    <button type="submit">{{ trans('index.search') }}</button>
+
+                    @if(session('locale') == 'ar')
+                        <button type="submit">{{ trans('index.search') }}</button>
+                        <input type="text" placeholder="{{ trans('index.find_your_catalog') }}"  name="search">
+                    @else
+                        <input type="text" placeholder="{{ trans('index.find_your_catalog') }}"  name="search">
+                        <button type="submit">{{ trans('index.search') }}</button>
+                    @endif
+
                 </form>
             </div>
         </div>
