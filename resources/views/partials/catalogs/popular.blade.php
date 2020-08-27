818e042e9692bfd53936753b828ff5efb89647b0
alpharameeztech <rameezisrarulhaq@gmail.com>
2020-08-27 21:17:04 +0500

    Show the catalog as big icon on the right side on arabic language

--- resources/views/partials/catalogs/popular.blade.php
+++ resources/views/partials/catalogs/popular.blade.php
@@ -4,7 +4,7 @@
         @if($catalog->ifExistInTheCity())
 
 
-            @if($key == 0)
+            @if($key == 0 && session('locale') == 'en')
             <div class="col-sm-6">
 
                 <div class="row">
@@ -34,6 +34,53 @@
                                         <span> - {{ \Carbon\Carbon::parse($catalog->end_at_at)->day }}
                                             {{ \Carbon\Carbon::parse($catalog->end_at)->subMonth()->format('F') }}
                                         </span>
+                                    @endif
+                                </p>
+                                <p>
+                                    Store {{$catalog->store->name}}
+                                    {{--  <a  href="/store/{{$catalog->store->slug}}" class="no-underline hover:underline text-blue-400">
+                                        {{$catalog->store->name}}
+                                    </a>  --}}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+            @endif
+
+            @if($key == 2 && session('locale') == 'ar')
+            <div class="col-sm-6">
+
+                <div class="row">
+                    <div class="col-sm-7">
+                        @foreach ($catalog->images as $image)
+
+                            @if ($image->featured)
+                                <a href="/{{session('locale')}}/catalog/{{$catalog->slug}}">
+                                    <img class="w-full" src="https://ecatalog.s3-ap-southeast-1.amazonaws.com/{{$image->image}}" alt="Sunset in the mountains">
+                                </a>
+                            @endif
+
+                        @endforeach
+
+                    </div>
+                    <div class="col-sm-5 catalogDetails topPopular">
+                        <div class="col-sm-12">
+                            <p class="catalogName">{{$catalog->name}}</p>
+                            <div class="textContainer">
+                                <p class="catalogDate">
+                                    {{ \Carbon\Carbon::parse($catalog->start_at)->day }}
+                                    @if(!$catalog->end_at)
+                                        {{ \Carbon\Carbon::parse($catalog->start_at)->subMonth()->format('F') }}
+                                    @endif
+
+                                    @if($catalog->end_at)
+                                        <span> - {{ \Carbon\Carbon::parse($catalog->end_at_at)->day }}
+                                            {{ \Carbon\Carbon::parse($catalog->end_at)->subMonth()->format('F') }}
+                                    </span>
                                     @endif
                                 </p>
                                 <p>
