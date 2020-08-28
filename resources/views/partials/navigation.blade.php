e886628049171fe7434bf145ae01ec8d32f633a2
alpharameeztech <rameezisrarulhaq@gmail.com>
2020-08-29 00:34:07 +0500

    Create a navigation layout for the arabic version

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,10 +1,13 @@
-<!--Navbar-->
 <style>
     .navbar-brand img{
         width: 50%;
     }
 
 </style>
+
+@if(session('locale') == 'en')
+    <!--Navbar English-->
+
     <nav class="customMainNav navbar navbar-expand-lg navbar-dark primary-color">
         <div class="container">
             <!-- Navbar brand -->
@@ -77,7 +80,7 @@
                                 <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
                                    aria-haspopup="true" aria-expanded="false">{{ucfirst($current_city->name)}}</a>
 
-                                @else
+                            @else
                                 <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
                                    aria-haspopup="true" aria-expanded="false">{{ trans('index.city') }}</a>
                             @endisset
@@ -124,4 +127,135 @@
             <!-- Collapsible content -->
         </div>
     </nav>
-<!--/.Navbar-->
+    <!--/.Navbar English-->
+@else
+    <!--Navbar Arabic-->
+
+    <nav class="customMainNav navbar navbar-expand-lg navbar-dark primary-color">
+        <div class="container">
+
+            <!-- Collapse button -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+                    aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Collapsible content -->
+            <div class="collapse navbar-collapse" id="basicExampleNav">
+
+                <!-- Links -->
+                <ul class="navbar-nav mr-auto">
+
+                    <!-- Dropdown -->
+                    <div>
+                        <li class="nav-item dropdown">
+                            @if(session('locale'))
+                                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                                   aria-haspopup="true" aria-expanded="false">{{ucfirst(session('locale'))}}</a>
+
+                            @else
+                                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                                   aria-haspopup="true" aria-expanded="false">Language</a>
+                            @endif
+
+
+                            <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+
+                                <a class="dropdown-item" href="/language/en">English</a>
+                                <a class="dropdown-item" href="/language/ar">Arabic</a>
+                            </div>
+                        </li>
+                    </div>
+
+                    <!-- Dropdown -->
+                    <div>
+                        <li class="nav-item dropdown">
+                            @isset($current_city)
+                                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                                   aria-haspopup="true" aria-expanded="false">{{ucfirst($current_city->name)}}</a>
+
+                            @else
+                                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                                   aria-haspopup="true" aria-expanded="false">{{ trans('index.city') }}</a>
+                            @endisset
+
+                            <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+
+                                <a class="dropdown-item" href="/">All</a>
+                                @if(count($all_cites) > 0)
+
+                                    @foreach($all_cites as $city)
+                                        <a class="dropdown-item" href="/{{session('locale')}}/city/{{$city->slug}}">{{$city->name}}</a>
+                                    @endforeach
+                                @endif
+
+                            </div>
+                        </li>
+                    </div>
+
+                    <!-- Country Dropdown -->
+                    <div>
+                        <li class="nav-item dropdown">
+                            @if(isset($current_country))
+                                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                                   aria-haspopup="true" aria-expanded="false">{{ucfirst($current_country->name)}}
+                                </a>
+
+                            @else
+                                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                                   aria-haspopup="true" aria-expanded="false">{{ trans('index.country') }}</a>
+                            @endif
+
+                            <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+
+                                <a class="dropdown-item" href="/">All</a>
+                                @if(count($all_countries) > 0)
+
+                                    @foreach($all_countries as $country)
+                                        <a class="dropdown-item" href="/{{session('locale')}}/country/{{$country->slug}}">
+                                            <img src="https://ecatalog.s3-ap-southeast-1.amazonaws.com/{{$country->image}}" class="country d-block w-100" alt="...">
+                                        </a>
+                                    @endforeach
+                                @endif
+
+                            </div>
+                        </li>
+                    </div>
+
+                    <!-- Country Dropdown end -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="/{{ session('locale') }}/contact-us">{{ trans('index.contact_us') }}</a>
+                    </li>
+
+                    <li class="@if(request()->path() == 'blog') active @endif nav-item">
+                        <a class="nav-link" href="/{{ session('locale') }}/blog">{{ trans('index.blog') }}</a>
+                    </li>
+
+                    <li class="@if(request()->path() == 'stores') active @endif nav-item">
+                        <a class="nav-link" href="/{{ session('locale') }}/stores">{{ trans('index.stores')}}</a>
+                    </li>
+
+                    <li class="@if(request()->path() == 'catalogs') active @endif nav-item">
+                        <a class="nav-link" href="/{{ session('locale') }}/catalogs">{{ trans('index.catalogs')}}</a>
+                    </li>
+
+                    <li class="@if(request()->path() == '/') active @endif  nav-item">
+                        <a class="nav-link" href="/">{{ trans('index.home')}} </a>
+                    </li>
+
+                </ul>
+                <!-- Links -->
+            </div>
+            <!-- Collapsible content -->
+
+            <!-- Navbar brand -->
+            <a class="navbar-brand textAlignRight" href="/">
+                <img src="/img/logo.png" />
+            </a>
+            <!-- Navbar brand end-->
+
+        </div>
+    </nav>
+    <!--/.Navbar Arabic-->
+@endif
+
