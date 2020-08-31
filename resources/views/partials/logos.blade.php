2664b43c648e273cb1f36e34030d4cca1a6a551c
alpharameeztech <rameezisrarulhaq@gmail.com>
2020-09-01 00:43:38 +0500

    Style the store logos on the home page

--- resources/views/partials/logos.blade.php
+++ resources/views/partials/logos.blade.php
@@ -1,17 +1,33 @@
 <div class="container">
-
-    <div class="row homeLogos">
+    <!--  only for extra small -->
+    <div class="d-flex d-sm-none row">
         @foreach ($stores as $store)
         @if ($store->featured)
-            <div class="col-sm-2">
+            <div class="col-6">
                 <a href="/store/{{$store->slug}}">
-                    <img class="w-full" src="https://ecatalog.s3-ap-southeast-1.amazonaws.com/{{$store->image}}" alt="Sunset in the mountains">
+                    <img class="w-full storeLogoForSmallScreen" src="https://ecatalog.s3-ap-southeast-1.amazonaws.com/{{$store->image}}" alt="Sunset in the mountains">
                 </a>
             </div>
         @endif
 
     @endforeach
     </div>
+    <!--  only for extra small -->
+
+    <!--  only for other than extra small -->
+    <div class="d-none d-sm-block row homeLogos logosLargeSection">
+        @foreach ($stores as $store)
+            @if ($store->featured)
+                <div class="col-sm-2">
+                    <a href="/store/{{$store->slug}}">
+                        <img class="w-full" src="https://ecatalog.s3-ap-southeast-1.amazonaws.com/{{$store->image}}" alt="Sunset in the mountains">
+                    </a>
+                </div>
+            @endif
+
+        @endforeach
+    </div>
+    <!--  only for other than extra small -->
 
 
 </div>
