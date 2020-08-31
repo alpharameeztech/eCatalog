772cbf9383181814e48516b731cbd949e56bd74f
alpharameeztech <rameezisrarulhaq@gmail.com>
2020-09-01 01:22:06 +0500

    Update the layout of the about us content on the mobile screen

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -1,5 +1,7 @@
 <div class="about">
-    <h2 class="@if(session('locale') == 'ar') textAlignRight @endif">{{ trans('index.about') }}</h2>
+    <h2 class="d-none d-sm-block @if(session('locale') == 'ar') textAlignRight @endif">{{ trans('index.about') }}</h2>
+    <h2 class="d-block d-sm-none textAlignCenter @if(session('locale') == 'ar') textAlignRight @endif">{{ trans('index.about') }}</h2>
+
     <div class="container">
         @foreach ($latest_blog as $blog)
         <div class="row">
