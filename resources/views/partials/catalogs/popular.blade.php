ef0698c43f7298501db3ad2b914bae9962dbb465
alpharameeztech <rameezisrarulhaq@gmail.com>
2020-09-01 01:04:38 +0500

    Show the popular catalogs two in each row on smaller screen

--- resources/views/partials/catalogs/popular.blade.php
+++ resources/views/partials/catalogs/popular.blade.php
@@ -1,6 +1,8 @@
 <h2 class="d-none d-sm-block @if(session('locale') == 'ar') textAlignRight @endif">{{ trans('index.the_most_popular_catalogs') }}</h2>
-<h2 class="d-block d-sm-none textAlignRight @if(session('locale') == 'ar') textAlignRight @endif">{{ trans('index.the_most_popular_catalogs') }}</h2>
-<div class="row popularCatalogsContainer">
+<h2 class="d-block d-sm-none textAlignCenter @if(session('locale') == 'ar') textAlignRight @endif">{{ trans('index.the_most_popular_catalogs') }}</h2>
+
+<!-- for greater than smaller screens -->
+<div class="d-none d-sm-flex row popularCatalogsContainer">
     @foreach ($popular_catalogs as $key=>$catalog)
         @if($catalog->ifExistInTheCity())
 
@@ -144,3 +146,58 @@
     @endforeach
 
 </div>
+<!-- for greater than smaller screens end -->
+
+
+<!-- for extra small screens -->
+<div class="d-flex d-sm-none row ">
+    @foreach ($popular_catalogs as $key=>$catalog)
+        @if($catalog->ifExistInTheCity())
+
+            <div class="col-6">
+
+                @foreach ($catalog->images as $image)
+
+                    @if ($image->featured)
+                        <a href="/{{session('locale')}}/catalog/{{$catalog->slug}}">
+                            <img class="w-full" src="https://ecatalog.s3-ap-southeast-1.amazonaws.com/{{$image->image}}" alt="Sunset in the mountains">
+                        </a>
+                    @endif
+
+                @endforeach
+
+                <div class="row catalogDetails">
+                    <div class="col-sm-12">
+                        <p class="catalogName @if(session('locale') == 'ar') textAlignRight @endif">{{$catalog->name}}</p>
+                        <div class="textContainer">
+                            <p class="catalogDate @if(session('locale') == 'ar') textAlignRight @endif">
+                                {{ \Carbon\Carbon::parse($catalog->start_at)->day }}
+                                @if(!$catalog->end_at)
+                                    {{ \Carbon\Carbon::parse($catalog->start_at)->subMonth()->format('F') }}
+                                @endif
+
+                                @if($catalog->end_at)
+                                    <span> - {{ \Carbon\Carbon::parse($catalog->end_at_at)->day }}
+                                        {{ \Carbon\Carbon::parse($catalog->end_at)->subMonth()->format('F') }}
+                                    </span>
+                                @endif
+                            </p>
+                            <p class="@if(session('locale') == 'ar') textAlignRight @endif">
+                                {{ trans('index.store') }} {{$catalog->store->name}}
+                                {{--  <a  href="/store/{{$catalog->store->slug}}" class="no-underline hover:underline text-blue-400">
+                                    {{$catalog->store->name}}
+                                </a>  --}}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        @endif
+
+    @endforeach
+
+</div>
+
+<!-- for extra small screens end -->
