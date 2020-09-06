000473a4255cbea89da14b911dc8091db5da7ff5
alpharameeztech <rameezisrarulhaq@gmail.com>
2020-09-06 21:59:47 +0500

    Add a footer for small screens on english version

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -42,7 +42,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a href="/{{session('locale')}}/contact-us">
                             <div>
                                 {{ trans('index.contact_us')}}
                             </div>
@@ -98,7 +98,92 @@
 
         </div>
         <!-- for screen greater than extra small end-->
-    @else
+
+        <!-- only for extra small -->
+            <div class="d-block d-sm-none container">
+
+                    <div class="row">
+                        <div class="col-6 displayBlock navigationLinksForExtraSmallScreen">
+
+                            <a class="displayBlock darkGray" href="/">
+                                {{ trans('index.home')}}
+                            </a>
+
+                            <a class="displayBlock darkGray"  href="/stores">
+                                {{ trans('index.stores')}}
+                            </a>
+
+                            <a class="displayBlock darkGray"  href="/catalogs">
+                                {{ trans('index.catalogs')}}
+                            </a>
+
+                            <a class="displayBlock darkGray"  href="/{{session('locale')}}/about-us">
+                                {{ trans('index.about')}}
+                            </a>
+
+                        </div>
+
+                        <div class="col-6 navigationLinksForExtraSmallScreen">
+                            <a class="darkGray displayBlock" href="/faq">
+                                <div>
+                                    {{ trans('index.faq')}}
+                                </div>
+                            </a>
+                            <a class="darkGray displayBlock" href="/{{session('locale')}}/contact-us">
+                                <div>
+                                    {{ trans('index.contact_us')}}
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+                <div class="row subHeading">
+                    <div class="col-sm-3">
+                        <p class="subHeading2">{{ trans('index.stores')}}</p>
+
+                        @foreach ($recent_stores as $store)
+                            <p><a href="/store/{{$store->slug}}">{{$store->name}}</a></p>
+                        @endforeach
+
+                        <p><a href="">{{ trans('index.all_stores') }}</a></p>
+
+                    </div>
+                    <div class="col-sm-3">
+                        <p class="subHeading2">{{ trans('index.cities') }}</p>
+
+                        @foreach ($recent_cities as $city)
+
+                            <p><a class="@if(!empty (request('city') && request('city') == $city->slug )) active @endif" href="/{{session('locale')}}/city/{{$city->slug}}">{{$city->name}}</a></p>
+
+                        @endforeach
+
+                        <p><a href="/">{{ trans('index.all_cities')}}</a></p>
+
+                    </div>
+                    <div class="col-sm-3">
+
+                        <p class="subHeading2"><a href="/terms">{{ trans('index.terms') }}</a></p>
+
+                    </div>
+                    <div class="col-sm-3">
+                        <p class="subHeading2">{{ trans('index.follow') }}</p>
+
+                        <div class="social">
+                            @if($social)
+                                <a href="{{$social->facebook}}"><img src="/img/icons/facebook.svg" /></a>
+                                <a href="{{$social->twitter}}"><img src="/img/icons/twitter.svg" /></a>
+                                <a href="{{$social->instagram}}"><img src="/img/icons/instagram.svg" /></a>
+                                <a href="{{$social->youtube}}"><img src="/img/icons/youtube.svg" /></a>
+                            @endif
+
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!-- only for extra small end-->
+        @else
         <div class="container">
 
             <nav role='navigation' class="main-nav" id="main-nav">
