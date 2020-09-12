7198146095ecd7d015f8b1fa51676b2797c84275
alpharameeztech <rameezisrarulhaq@gmail.com>
2020-09-12 18:54:37 +0500

    Update the layout of single blog page for mobile screens

--- resources/views/pages/blog/show.blade.php
+++ resources/views/pages/blog/show.blade.php
@@ -106,8 +106,9 @@
     <!-- advertisements end -->
 
         {{--        ===================== popular blog posts =================--}}
-        <h2 class="lineBreaker @if(session('locale') == 'ar') textAlignRight @endif">{{trans('index.the_most_popular_catalogs')}}</h2>
-
+        <h2 class="d-none d-sm-block lineBreaker @if(session('locale') == 'ar') textAlignRight @endif">{{trans('index.the_most_popular_catalogs')}}</h2>
+        <h2 class="d-block d-sm-none textAlignCenter lineBreaker @if(session('locale') == 'ar') textAlignRight @endif">{{trans('index.the_most_popular_catalogs')}}</h2>
+        
         <div class="row">
             @foreach($popular_blogs as $blog)
                 <div class="col-sm-6">
