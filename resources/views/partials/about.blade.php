95500fe55c08e3587027e29b1965c796eab07331
alpharameeztech <rameezisrarulhaq@gmail.com>
2020-09-24 23:36:18 +0500

    Add the about us page link

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -1,6 +1,14 @@
 <div class="about">
-    <h2 class="d-none d-sm-block @if(session('locale') == 'ar') textAlignRight @endif">{{ trans('index.about') }}</h2>
-    <h2 class="d-block d-sm-none textAlignCenter @if(session('locale') == 'ar') textAlignRight @endif">{{ trans('index.about') }}</h2>
+    <h2 class="d-none d-sm-block @if(session('locale') == 'ar') textAlignRight @endif">
+        <a href="/{{ session('locale') }}/about-us" style="color:#000">
+            {{ trans('index.about') }}
+        </a>
+    </h2>
+    <h2 class="d-block d-sm-none textAlignCenter @if(session('locale') == 'ar') textAlignRight @endif">
+        <a href="/{{ session('locale') }}/about-us" style="color:#000">
+            {{ trans('index.about') }}
+        </a>
+    </h2>
 
     <div class="container">
         @foreach ($latest_blog as $blog)
@@ -20,7 +28,9 @@
 
             </div>
             <div class="col-sm-5">
+            <a href="/{{ session('locale') }}/about-us" style="color:#000">
                 <img src="/img/main-page/about.png" />
+            </a>
             </div>
 
         </div>
