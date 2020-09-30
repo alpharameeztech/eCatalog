540a32fc4e6b72a9dcc055bffb170419887a1926
alpharameeztech <rameezisrarulhaq@gmail.com>
2020-09-30 19:09:41 +0500

    Show the pdf only on the arabic version if the pdf file exist on the catalog

--- resources/views/pages/catalog/show.blade.php
+++ resources/views/pages/catalog/show.blade.php
@@ -800,11 +800,13 @@
 
                                     @foreach($catalog->pdfs as $pdf)
                                         <td colspan="2">
-                                            <a target="_blanck" href="https://ecatalog.s3-ap-southeast-1.amazonaws.com/{{$pdf->pdf}}">
+                                            {{--  <a target="_blanck" href="https://ecatalog.s3-ap-southeast-1.amazonaws.com/{{$pdf->pdf}}">
                                                 <img id="image" class="w-full"
                                                      src="https://ecatalog.s3-ap-southeast-1.amazonaws.com/{{$image->image}}"
                                                      alt="Sunset in the mountains">
-                                            </a>
+                                            </a>  --}}
+                                    <div class="flip-book-container" src="{{ asset('storage/'.$pdf->pdf) }}"></div>
+
                                         </td>
 
 
@@ -815,9 +817,9 @@
 
                                 <p class="textCenter">
                                     @foreach($catalog->pdfs as $pdf)
-                                    <div class="flip-book-container" src="https://ecatalog.s3-ap-southeast-1.amazonaws.com/{{$pdf->pdf}}"></div>
+                                        {{--  <a target="_blank" href="https://ecatalog.s3-ap-southeast-1.amazonaws.com/{{$pdf->pdf}}">Click here to view</a>  --}}
+                                    <div class="flip-book-container" src="{{ asset('storage/'.$pdf->pdf) }}"></div>
 
-                                        <a target="_blank" href="https://ecatalog.s3-ap-southeast-1.amazonaws.com/{{$pdf->pdf}}">Click here to view</a>
                                     @endforeach
                                 </p>
 
